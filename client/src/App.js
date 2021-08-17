0ba9bfc8ba951478378704054f2e1dda765f14f9
kennethchuson <kennethchuson@yahoo.com>
2021-08-16 23:23:31 -0700

    added settings feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React, { useState }  from 'react'
 import Calendar from './components/calendar.js';
+import Settings from './components/settings.js'; 
 import {ThemeProvider} from "styled-components";
 import { GlobalThemes } from "./themes/global-themes"; 
 import { LightTheme, DarkTheme } from "./themes/themes"; 
 import DarkModeToggle from "react-dark-mode-toggle";
+import Hamburger from 'hamburger-react'
+
+
+
 
 
 
 const App = () => {
 
     const [theme, setTheme] = useState(() => false);
+    const [settings, setShowSettings] = useState(false); 
 
+    const clickSettings = () => {
+        setShowSettings(true); 
+    }
 
 
     return (
@@ -19,7 +28,9 @@ const App = () => {
             <GlobalThemes/>
                 <div className="App">
                     <h1 className="App-title">Personal Calendar</h1>
-                    <DarkModeToggle onChange={setTheme} checked={theme} size={50}/>
+                    <DarkModeToggle onChange={setTheme} checked={theme} size={65}/>
+                    <Hamburger toggled={settings} toggle={setShowSettings} />
+                    { settings? <Settings value_one={setTheme} value_two={theme} /> : null }
                     <Calendar/>
                 </div>
             </>
---
+++ client/src/components/settings.js
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react'
+
+const Settings = (props) => {
+    return (
+        <div className="settings-wrapper">
+            <h3 className="settings-title">Settings</h3>
+            <div className="settings-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Settings; 
